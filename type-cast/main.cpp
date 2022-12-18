c51d9dc63efcd29ae700b35efdb2d8946592056f
hzh1423443710 <1423443710@qq.com>
2022-12-18 16:53:06 +0800

    类型转换

--- type-cast/main.cpp
+++ type-cast/main.cpp
@@ -3,9 +3,16 @@ using namespace std;
 
 int main()
 {
+    //1.const_cast :const int* -> int*
     const int i = 100;
-    int* p = const_cast<int*>(&i);//c++常类型转换
+    int* p = const_cast<int*>(&i);
                                   
+    //2.dynamic_cast 父类指针指向子类,不加也可以,自动转
+
+
+    //3.static_cast 子类指针指向父类 
+
+    //4.reinterpret_cast :相当于C语言的强转,不同类型之间的强转 int* -> char*
     char buff[] = "0001\00012345\0001234567\000";
     struct A
     {
@@ -14,7 +21,7 @@ int main()
         char c[8];
     };
     //A* pa  = (A*)buff;
-    A* pa  = reinterpret_cast<A*>(buff);//c++重新解释类型转换
+    A* pa  = reinterpret_cast<A*>(buff);
     cout << pa->a << endl;
     cout << pa->b << endl;
     cout << pa->c << endl;
