412ea1bf50a66f826d90b33f80d6a441368f4e52
hzh1423443710 <1423443710@qq.com>
2023-11-12 11:47:53 +0800

    实现std::function

--- Features/function/main.cpp
+++ Features/function/main.cpp
@@ -1,85 +1,161 @@
-#include <functional>
 #include <iostream>
 #include <memory>
+#include <functional>
 #include <type_traits>
 
-template <class FunSig>
+template <class Signature>
 class Function {
-	static_assert(!std::is_same_v<FunSig, FunSig>, "should pass function sigature");
+	static_assert(!std::is_same_v<Signature, Signature>,
+				  "incorret function signature");
 };
 
 template <class Ret, class... Args>
 class Function<Ret(Args...)> {
 private:
-	class Callable {
+	class FunctorBase {
 	public:
-		virtual Ret call(Args... arg) = 0;
-		virtual ~Callable() {}
+		virtual FunctorBase* clone() const noexcept = 0;
+		virtual Ret call(Args...) = 0;
+		virtual ~FunctorBase() {}
 	};
 
-	template <class Functor>
-	class ConcreteCallable : public Callable {
+	template <class Fn>
+	class FunctorImpl : public FunctorBase {
 	public:
-		ConcreteCallable(const Functor& fn) : m_f(fn) {}
-		Ret call(Args... arg) override { return m_f(arg...); }
+		FunctorImpl(const Fn& fn) : m_fn(fn) {}
+		FunctorImpl* clone() const noexcept override {
+			return new FunctorImpl<Fn>(m_fn);
+		}
+		Ret call(Args... args) override { m_fn(args...); }
 
 	private:
-		Functor m_f;
+		Fn m_fn;
 	};
 
 public:
-	// 构造
+	// 默认构造
 	Function() = default;
-	template <class Functor>
-	Function(Functor&& f)
-		: m_ptr(std::make_unique<ConcreteCallable<Functor>>(std::forward<Functor>(f))) {}
 
-	// 调用
-	template <class... T>
-	Ret operator()(T... args) const {
-		if (!m_ptr) throw std::runtime_error("Uninitialized Function");
-		return m_ptr->call(std::forward<T>(args)...);
+	// 构造空的Funciton
+	Function(std::nullptr_t) : Function() {
+		std::cout << "Function(std::nullptr_t)\n";
 	}
 
-	// 赋值
-	template <class Functor>
-	Function& operator=(Functor&& f) {
-		Function{f}.m_ptr.swap(this->m_ptr);
+	// 构造Callable Object
+	template <class Fn, class = std::enable_if_t<
+							std::is_invocable_r_v<Ret, Fn, Args...> &&
+								!std::is_same_v<std::decay_t<Fn>, Function>,
+							void>>
+	Function(Fn&& fn)
+		: m_ptr(std::make_unique<FunctorImpl<Fn>>(std::forward<Fn>(fn))) {
+		std::cout << "Function(Fn&&)\n";
+	}
+
+	// 拷贝构造
+	Function(const Function& other) {
+		std::cout << "Function(const Function&)\n";
+		// 从基类拷贝一个实体
+		if (other) m_ptr.reset(other.m_ptr->clone());
+	}
+
+	// 移动构造
+	Function(Function&& other) noexcept {
+		std::cout << "Function(Function&&)\n";
+		if (other) m_ptr.swap(other.m_ptr);
+	}
+
+	// 调用
+	Ret operator()(Args... args) const {
+		if (m_ptr)
+			return m_ptr->call(std::forward<Args>(args)...);
+		else
+			throw std::runtime_error("Uninitialized Function");
+	}
+
+	// 拷贝赋值
+	Function& operator=(const Function& rhs) {
+		if (this != &rhs) Function(rhs).m_ptr.swap(m_ptr);
 		return *this;
 	}
 
+	// 移动赋值
+	Function& operator=(Function&& rhs) noexcept {
+		if (this != &rhs) Function(std::move(rhs)).m_ptr.swap(m_ptr);
+		return *this;
+	}
+
+	// 存储functor
+	template <class Fn>
+	std::enable_if_t<std::is_invocable_r_v<Ret, Fn, Args...> &&
+						 !std::is_same_v<std::decay_t<Fn>, Function>,
+					 Function&>
+	operator=(Fn&& fn) {
+		std::cout << "operator=(Fn&&)\n";
+		this->m_ptr.reset(new FunctorImpl<Fn>(std::forward<Fn>(fn)));
+		return *this;
+	}
+
+	// 置空
 	Function& operator=(std::nullptr_t) {
 		m_ptr.reset();
 		return *this;
 	}
 
+	// operator bool
+	explicit operator bool() const noexcept { return m_ptr != nullptr; }
+
+	bool empty() const noexcept { return m_ptr == nullptr; }
+
 private:
-	std::unique_ptr<Callable> m_ptr;
+	std::unique_ptr<FunctorBase> m_ptr;
 };
 
-void print1(const char* str) { std::cout << str << "\n"; }
-void print2(const char* str) { std::cout << "#" << str << "\n"; }
+void printMsg(const std::string& msg) { std::cout << "PRINT: " << msg << "\n"; }
 
-struct D {
-	void operator()(const char* str) { std::cout << __PRETTY_FUNCTION__ << ":" << str << "\n"; }
-};
+void test_Function() {
+	using Signature = void(const std::string&);
+	Function<Signature> f0 = nullptr;		// nullptr构造
+	Function<Signature> f1 = f0;			// 拷贝构造
+	Function<Signature> f2 = std::move(f1); // 移动构造
 
-int main() {
-	Function<void(const char*)> f = print1;
-	f("1212");
+	// 可调用对象构造
+	Function<Signature> f = printMsg;
+	f("Callable object construct: f is printMsg");
 
-	f = D{};
-	f("HHHH");
+	// 可调用对象赋值
+	f = [](const std::string& str) { std::cout << "Lambda: " << str << "\n"; };
+	f("Callable object assigment: f is Lambda");
 
-	try {
-		f = nullptr;
-		f("12345");
-	} catch (const std::exception& e) {
-		std::cout << "exception:" << e.what() << "\n";
-	}
+	// 空赋值
+	f = nullptr;
+	if (f.empty()) std::cout << "nullptr assignment: f is nullptr";
 
-	f = [](const char* str) { std::cout << __PRETTY_FUNCTION__ << "\n"; };
-	f("Lambda");
+	// 拷贝赋值
+	f = f0;
+	if (f.empty()) std::cout << "copy assignment: f is empty function";
+
+	// 移动赋值
+	f = std::move(f0);
+	if (f.empty()) std::cout << "move assignment: f is empty function";
+}
+
+void test_contravariant() {
+	// std::function
+	std::function<void(const char*)> f0;
+	std::function<void(const std::string&)> f1;
+	f0 = f1; // correct		f1的Callable object可以调用void(const char*)
+	// f1 = f0; // incorrect	f0的Callable object不能调用void(const string&)
+
+	// my function
+	Function<void(const char*)> ff0;
+	Function<void(const std::string&)> ff1;
+	ff0 = ff1; // correct		ff1的Callable object可以调用void(const char*)
+	// ff1 = ff0; // incorrect	ff0的Callable object不能调用void(conststring&)
+}
+
+int main(int argc, char* argv[]) {
+	test_Function();
+	test_contravariant(); // 逆变
 
 	return 0;
 }
